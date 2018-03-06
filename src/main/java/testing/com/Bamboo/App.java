53b435f80ca3fd3f57f67b1a5d03d96b9440cb96
RKUMAR16144 <raju.kumar@pwc.com>
2018-03-06 12:28:39 +0530

    first commit

---
+++ src/main/java/testing/com/Bamboo/App.java
@@ -0,0 +1,15 @@
+package testing.com.Bamboo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/testing/com/Bamboo/AppTest.java
@@ -0,0 +1,27 @@
+package testing.com.Bamboo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+        System.out.println("Test begins");
+    }
+
+
+ 
+}
+
